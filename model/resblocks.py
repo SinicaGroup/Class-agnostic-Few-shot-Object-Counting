ac3046a17347b14ad29d914cffaea502d5c78f79
Hacker-Davinci <76461262+Hacker-Davinci@users.noreply.github.com>
2022-09-22 17:12:09 +0800

    Update resblocks.py

--- model/resblocks.py
+++ model/resblocks.py
@@ -2,7 +2,15 @@ from torchvision.models.resnet import resnet50
 import torch.nn as nn
 
 def make_resblocks():
-    net = resnet50()
+    
+    # no pretrained weights
+    net = resnet50(pretrained=False)
+
+    # Use it if you want to freeze backbone
+    # net = resnet50(pretrained=True)
+    # for param in net.parameters():
+    #     param.requires_grad = False
+    
     layer0_name = ['conv1','bn1','relu']
     layer1_name = ['maxpool','layer1']
     layer2_name = ['layer2']
